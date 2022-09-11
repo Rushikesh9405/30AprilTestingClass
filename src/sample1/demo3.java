b76723be785341cf51a3e55b2f96e9a4497e5593
HP <HP@DESKTOP-DKRK8K1>
2022-09-11 16:00:51 +0530

    Rushikesh created three classes

---
+++ src/sample1/demo3.java
@@ -0,0 +1,13 @@
+package sample1;
+
+public class demo3 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("how are you ");
+		
+		System.out.println("I love you");
+		
+	}
+
+}
